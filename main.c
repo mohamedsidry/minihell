eb74fc427f517b30eeb158ffe8fed432e5968548
Mohamed Sidry <msidry@c2r8p6.1337.ma>
2025-10-10 11:03:53 +0100

    fix (signal) : fix ctl + c exit code !

--- main.c
+++ main.c
@@ -6,13 +6,15 @@
 /*   By: msidry <msidry@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/05 21:59:37 by azghibat          #+#    #+#             */
-/*   Updated: 2025/10/09 12:49:19 by msidry           ###   ########.fr       */
+/*   Updated: 2025/10/10 11:02:38 by msidry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/main.h"
 
 int	g_signal_receiv = 0;
+
+static void	ctr_c_handler(int *error);
 
 void	sg_handler(int sig_num)
 {
@@ -43,10 +45,18 @@ int	main(int argc, char *argv[], char *env[])
 	while (1)
 	{
 		proreadline(&input, &new_env, &error);
+		ctr_c_handler(&error);
 		lexer(&commands, &input, &new_env, &error);
 		executor(&commands, &new_env, &error);
 		cmd_clear(&commands);
 		nullstr(&input);
 	}
 	return (0);
+}
+
+static void	ctr_c_handler(int *error)
+{
+	if (g_signal_receiv == SIGINT)
+		*error = 1;
+	g_signal_receiv = 0;
 }
